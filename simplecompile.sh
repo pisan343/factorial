e1e2c5a57e40b781c8dcda7c924c93e4b7636527
Yusuf Pisan <pisan@uw.edu>
2019-01-10 19:49:10 -0800

    added dos2unix

--- simplecompile.sh
+++ simplecompile.sh
@@ -3,6 +3,11 @@
 # To more easily compile and run this program on CSS Linux Lab
 # Lines starting with '$' indicate what is typed on command line
 
+# if you get the following error:
+# -bash: ./simplecompile.sh: /bin/bash^M: bad interpreter: No such file or directory
+# run dos2unix to fix it
+# $ dos2unix simplecompile.sh
+
 # make this file executable
 # $ chmod 700 simplecompile.sh
 # redirect the output and stderr from this file to output.txt
